ebfc06cec856bd36fd350c678893dd7ddee529a6
Uzair Mohammed <uzair.hamed@gmail.com>
2024-12-04 22:28:25 -0800

    Initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+LazyVim.terminal.setup("pwsh -NoLogo")
+
+vim.opt.relativenumber = false -- Disable relative line numbers
